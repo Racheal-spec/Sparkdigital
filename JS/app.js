dcc65ab68764e10c97322a8922fdbee3b0ffe407
Racheal-spec <folorunshotosin334@gmail.com>
2020-08-03 16:33:57 +0100

    Added page transitions

--- JS/app.js
+++ JS/app.js
@@ -34,88 +34,32 @@ sliders.forEach(slide => {
 var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
 gsap.to(".hero-img", {duration: 1, scale: 1});
 */
-//Use to reset the position of the loader
-function init(){
-    
-    const loader = document.querySelector('.loader');
 
-    // reset position of the loading screen
-    gsap.set(loader, {
-        scaleX: 0, 
-        rotation: 10, 
-        xPercent: -5,
-        yPercent: -50, 
-        transformOrigin: 'left center', 
-        autoAlpha: 1
-    });
-
-    function loaderIn() {
-        // GSAP tween to stretch the loading screen across the whole screen
-        return gsap.fromTo(loader, 
-            {
-                rotation: 10,
-                scaleX: 0,
-                xPercent: -5
-            },
-            { 
-                duration: 0.8,
-                xPercent: 0,
-                scaleX: 1, 
-                rotation: 0,
-                ease: 'Power4.inOut', 
-                transformOrigin: 'left center'
-            });
-    }
-
-    function loaderAway() {
-        // GSAP tween to hide the loading screen
-        return gsap.to(loader, { 
-            duration: 0.8, 
-            scaleX: 0,
-            xPercent: 5, 
-            rotation: -10, 
-            transformOrigin: 'right center', 
-            ease: 'Power4.inOut'
-        });
-    }
-
-    // do something before the transition starts
-    barba.hooks.before(() => {
-
-        document.querySelector('html').classList.add('is-transitioning');
-        barba.wrapper.classList.add('is-animating');
-
-    });
-
-    // do something after the transition finishes
-    barba.hooks.after(() => {
-
-        document.querySelector('html').classList.remove('is-transitioning');
-        barba.wrapper.classList.remove('is-animating');
-
-    });
-
-    // scroll to the top of the page
-    barba.hooks.enter(() => {
-
-        window.scrollTo(0, 0);
-
-    });
-
-    barba.init({
-        transitions: [{
-            async leave() {
-                await loaderIn();
-        
-            },
-            enter() {
-                loaderAway();
-            }
-        }]
-    })
-
+function delay(n){
+    n = n || 2000;
+    return new promise(done => {
+        setTimeout(()=>{
+            done();
+        }, n);
+    }); 
 }
 
-window.addEventListener('load', function(){
-    init();
-});
+function pgTransition() {
+    var tl = gsap.timeline();
+    tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .2, delay: .1})
+}
+
+
+barba.init({
+    sync: true,
+    transitions: [{
+        async leave(data){
+            const done = this.async();
+
+            pgTransition();
+            await delay(1500);
+            done();
+        }
+    }]
+})
